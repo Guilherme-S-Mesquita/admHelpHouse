120fdcd29fce97a6e9ff0bbcbca95e4a1b5366d4
Guilherme Mesquita <guimesquita.1512@gmail.com>
2024-07-18 16:46:53 -0300

    Foi feito contador na dashboard, e dois models

--- adminHelpHouse/resources/views/layouts/main.blade.php
+++ adminHelpHouse/resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <!-- CSS da aplicação e JS -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
@@ -120,6 +121,8 @@
 
     <script src="{{ asset('js/main.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script src="{{ asset('js/chart.js') }}"></script>
+
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
