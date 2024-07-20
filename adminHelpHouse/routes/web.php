399b293c7784528c44c5eb88d9dd4113dc3963b0
Guilherme Mesquita <guimesquita.1512@gmail.com>
2024-07-20 16:27:29 -0300

    arrumei o route

--- adminHelpHouse/routes/web.php
+++ adminHelpHouse/routes/web.php
@@ -8,16 +8,13 @@ use App\Http\Controllers\ServicoController;
 Route::get('/admin/DashboardAdmin', [AdminController::class, 'index']);
 
 //todas as rotas para o login funcionar
-Route::controller(LoginController::class)->group(function (){
-Route::view('/', 'index')->name('login.index');
-Route::post('/login', 'store')->name('login.store');
-Route::get('/logout', 'destroy')->name('login.logout');
-});
+Route::get('/login',[LoginController::class, 'index'])->name('login.index');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('login.logout');
 
-Route::controller(ServicoController::class)->group(function (){
-Route::get('add/servico',  'servico')->name('add.servico');
-Route::post('/criarServico', 'store');
-});
+
+Route::get('add/servico',[ ServicoController::class ,'servico'])->name('add.servico');
+Route::post('/criarServico',[ServicoController::class, 'criarServico'])->name('criar.servico');
 
 
 
