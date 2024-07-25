1d3f6bc9ae9d9d97fcf84ac3aa5f5b6f63e0d0d3
Guilherme Mesquita <guimesquita.1512@gmail.com>
2024-07-25 15:31:20 -0300

    Fiz a tela do Login

--- adminHelpHouse/resources/views/login.blade.php
+++ adminHelpHouse/resources/views/login.blade.php
@@ -1,4 +1,41 @@
-
-<h1>salve</h1>
 <a href="{{ route('login.store') }}"></a>
-      
+
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<div class="wrapper">
+    <!-- Container principal -->
+    <div class="container main">
+        <!-- Linha para distribuição de conteúdo -->
+        <div class="row">
+            <!-- Coluna para o formulário de login -->
+            <div class="col-md-6 left">
+                <div class="input-box">
+                    <!-- Cabeçalho do formulário -->
+                    <header>Login</header>
+                    <!-- Campo de entrada para o email -->
+                    <div class="input-field">
+                        <input type="email" class="input" id="email" required="" autocomplete="off">
+                        <label for="email"><i class="fa-solid fa-envelope"></i>E-mail</label>
+                    </div>
+                    <!-- Campo de entrada para a senha -->
+                    <div class="input-field">
+                        <input type="password" class="input" id="pass" required="">
+                        <label for="pass"><i class="fa-solid fa-lock"></i> Senha</label>
+                    </div>
+                    <!-- Botão de envio do formulário -->
+                    <div class="input-field  ">
+                        <input type="submit" class="submit " value="Entrar">
+                    </div>
+                    <!-- Link para o cadastro -->
+                    <div class="signin">
+                        <span>Não tem uma conta? <a href="#">Faça o cadastro aqui!</a></span>
+                    </div>
+                </div>
+            </div>
+            <!-- Coluna para a imagem lateral -->
+            <div class="col-md-6 " id="side-image">
+                <!-- Adicione o texto sobre a imagem aqui se necessário -->
+               <img src="/img/helpHouseLogin.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
