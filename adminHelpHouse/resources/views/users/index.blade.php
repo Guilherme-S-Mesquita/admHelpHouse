9739e25ae7ab32e245e7dc5fecccf793feb429f3
JvTorelli <joaovictorelli7@gmail.com>
2024-11-06 14:01:44 -0300

    tela de usuarios

--- adminHelpHouse/resources/views/users/index.blade.php
+++ adminHelpHouse/resources/views/users/index.blade.php
@@ -6,33 +6,60 @@
 
 <div class="main p-3" style="height: 100vh;">
 
-    <div class="header mb-4 "   style="margin-left:3vh; margin-top: 10vh">
-       
-        <h2>Controle de Usuários</h2>
+    <div class="header mb-4 "    style="margin-left:3vh;">
+        <h1>Olá,<span style="color: #ff6347;">{{$user->name}}</span></h1>
+        <h2>Usuários</h2>
     </div>
 
+    <div class="container-fluid h-85 d-flex justify-content-center align-items-center"   style= "margin-left:2vh;">
+        <div class="row w-100 d-flex justify-content-around">
+          
+            <div class="col-md-4 mb-4">
+                <a href="{{route('users.clientes')}}" class="card-link">
+                    <div class="card3">
+                        <div class="card-body">
+                            <img src="/img/userPerfil.png" alt="Ícone de Clientes">
+                            <h3 class="card-title">Profissionais</h3>
+                        </div>
+                    </div>
+                </a>
+            </div>
 
-    <div class="tudo">
-       
-                <a href="{{route('users.admins')}}" >
-                    <div class="icon"><i class="bi bi-person-fill-gear"></i></div>
-                    <div class="titulo"><h2>Administradores</h2></div> 
+            <div class="col-md-4 mb-4">
+                <a href="#" class="card-link">
+                    <div class="card2">
+                        <div class="card-body2">
+                            <img src="/img/userPerfil.png" alt="Ícone de Profissionais">
+                            <h3 class="card-title2">Clientes</h3>
+                        </div>
+                    </div>
                 </a>
-       
-           
-                <a href="{{route('users.clientes')}}">
-                    <div class="icon"><i class="bi bi-people-fill"></i></div>
-                    <div class="titulo"><h2>Clientes</h2></div> 
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <a href="{{route('users.admins')}}" class="card-link">
+                    <div class="card">
+                        <div class="card-body3">
+                            <img src="/img/userPerfil.png" alt="Ícone de Administradores">
+                            <h3 class="card-title3">Administradores</h3>
+                        </div>
+                    </div>
                 </a>
-        
-      
-                <a href="{{route('users.clientes')}}">
-                    <div class="icon"><i class="bi bi-person-fill-up"></i></div>
-                    <div class="titulo"><h2>Profissionais</h2></div> 
-                </a>
-        
+            </div>
+
+        </div>
+    </div>
+    <div class="partes">
+    </div>
+    <div class="partes2">
     </div>
 </div>
 
+
+<style> 
+
+    
+
+</style>
 
 @endsection
